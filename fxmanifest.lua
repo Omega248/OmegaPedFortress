01df6a94d8d5de187a784f8a61894497ecf36504
Omega <86800507+OmegaGrape@users.noreply.github.com>
2022-04-13 13:45:06 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+description 'qb-gunsmission'
+version '1.0'
+author 'https://github.com/OmegaGrape'
+
+
+client_scripts {
+    'client/client.lua'
+}
+
+shared_script 'config.lua'
